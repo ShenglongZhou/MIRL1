cec9f1223a450ed5945878090b37750137d56dba
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-04-25 16:25:23 +0100

    Update SuccessRate_for_Gau_pDCT.m

--- MIRL1/SuccessRate_for_Gau_pDCT.m
+++ MIRL1/SuccessRate_for_Gau_pDCT.m
@@ -1,34 +1,32 @@
 clc; clear all;close all;
 % This file aims at generating recovery success rate.
 % Gaussian and  Partial DCT type measurement matrices, that will be tested.
-% proname{1} will generate the plot of success rate for Gaussian matrix
-% proname{2} will generate the plot of success rate for Partial DCT matrix
+% pbnm{1} will generate the plot of success rate for Gaussian matrix
+% pbnm{2} will generate the plot of success rate for Partial DCT matrix
 
 % Initialization
 addpath('MIRL1'); 
-m        = 64; 
-n        = 256; 
-k0       = 10:2:40; 
-Sample   = 100; 
-SuccRate = [];
-proname  = {'GaussianMat','PartialDCTMat'}; 
-problem  = proname{1}; % change here to test another type matrix
+m      = 64; 
+n      = 256; 
+k0     = 10:2:40; 
+Smpl   = 100; 
+test   = 1; % test =1, 2 
+pbnm   = {'GaussianMat','PartialDCTMat'}; 
+ScRt   = [];
 
 % Test examples
-for j=1:length(k0) 
+for j     = 1:length(k0) 
     rate  = 0;
-    for p = 1:Sample        
-    [A,b,x_opt ] = CSMatrix(problem, m,n,k0(j) ); 
-    opts.IterOn  = 0;
-    x            = MIRL1(A,b,opts);    
-    if norm(x-x_opt)/norm(x)<1e-2; rate=rate+1;end 
-    end       
-    SuccRate=[SuccRate rate/Sample]; clc; SuccRate
+    for p = 1: Smpl        
+        [A,b,x_opt] = CSMatrix(pbnm{test}, m,n,k0(j) ); 
+        opts.IterOn = 0;
+        x           = MIRL1(A,b,opts);    
+        rate        = rate+( norm(x-x_opt)/norm(x)<1e-2 ); 
+        end       
+    ScRt        = [ScRt rate/Smpl]; clc; ScRt
 end
 
 % Graph design
-plot(k0,SuccRate(1,:),'r*-');
-set(gca,'FontName','Times','FontSize',10)
-ylabel('Success rate') 
-xlabel('Sparsity')
-title([num2str(problem)])
+figure, plot(k0,ScRt,'r*-');
+ylabel('Success rate'), xlabel('Sparsity')
+title(pbnm{test}), grid on
