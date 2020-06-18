b837facfeeacae72fc8902742217e8b003dd3f3d
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-06-18 17:52:05 +0100

    Update Demon.m

--- MIRL1/Demon.m
+++ MIRL1/Demon.m
@@ -2,7 +2,7 @@ clc; clear all;close all;
 % This file aims at demonstrating MIRL1 for recovering the compressed
 % sensing problems, and be the supportive material for the article titled
 % "A Null-Space-Based Weighted l1 Minimization Approach to Compressed Sensing"
-% which can be downloaded at:
+% which is available at:
 % http://imaiai.oxfordjournals.org/content/early/2016/02/11/imaiai.iaw002
 
 % Gaussian, Partial DCT, Toeplitz Correlation and Over Sampled Partial DCT 
@@ -12,34 +12,28 @@ clc; clear all;close all;
 % Initialization 
 warning off, addpath('MIRL1');
 
-test = 1;
-m    = 1000; n = 4*m;  k = floor(0.01*n);
-if test == 4  % This is particularly for 'OverSamDCTMat' 
-m    = 100;  n = 2000; k = 10; 
+test  = 1;
+switch test
+  case {1,2,3}; m=1e3; n=4*m;  k=ceil(0.01*n);
+  case 4;       m=100; n=2000; k=10; %For 'OverSamDCTMat'     
 end
-
-proname  = {'GaussianMat','PartialDCTMat','ToeplitzCorMat','OverSamDCTMat'}; 
+proname  = {'GaussianMat',   'PartialDCTMat',...
+            'ToeplitzCorMat','OverSamDCTMat'}; 
 problem  = proname{test};
 
-% generate the data (A, b, x_opt)
-fprintf('\n Start to generate data... ');
-fprintf('\n ---------------------------------- ');
+% generate (A, b, x_opt)
+fprintf(' Generate data of %s...',problem); 
 [A,b,xopt] = CSMatrix(problem,m,n,k); 
 
-% call MIRL1 solver to solve the problem
-fprintf('\n Problem name: %s\n',problem);  
-fprintf(' Sample size:  n=%d, m=%d, k=%d\n \n Start to run...',n,m,k);
-fprintf('\n ---------------------------------- \n');
-opts.IterOn = 1;
-%opts.k      = k;
+% call MIRL1 solver 
+opts.k      = k;
 [x, Out]    = MIRL1(A,b,opts);
 relerr      = norm(x-xopt)/norm(x);
-clear A b xopt
-
-% result output
-fprintf('\n ---------------------------------- ');
-fprintf('\n Relative error  =  %1.3e ', relerr);
-fprintf('\n CPU time        =  %1.3f(sec)', Out.time);
-if relerr<1e-2; fprintf('\n Recovery is successful!\n\n');
-else;           fprintf('\n Recovery is unsuccessful!\n\n');
+ 
+fprintf(' CPU time:         %.3f(sec)\n', Out.time);
+fprintf(' Objective:        %5.3e\n', Out.obj);
+fprintf(' Relative error:   %5.3e\n', relerr);
+fprintf(' Sample size:      m=%d,n=%d,k=%d\n',m,n,k);
+if relerr<1e-2; fprintf(' Recovery is successful!\n\n');
+else;           fprintf(' Recovery is unsuccessful!\n\n');
 end
